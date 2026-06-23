f655dd1241a3b5f9554ad29fc1f936c75c1db80a
KavinthaBandara <kavinthahasaranga2@gmail.com>
2026-06-23 09:14:36 -0700

    URL for Destroyu

--- Todo/core/urls.py
+++ Todo/core/urls.py
@@ -1,12 +1,14 @@
 
 from django.urls import path
 
-from .views import AddTodo, ViewTodo
+from .views import AddTodo, TodoDestroyAPIView, ViewTodo
 
 
 urlpatterns = [
     path('addtodo/', AddTodo.as_view(), name='add_todo'),
     path('viewtodo/', ViewTodo.as_view(), name='View_todo'),
-    
+    path('<int: pk>/delete/', TodoDestroyAPIView.as_view(), name='todo-delete'),
+
+
 ]
 
