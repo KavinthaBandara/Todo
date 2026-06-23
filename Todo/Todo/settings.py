fa9a743d546ad807a957e1d28c3b4167ddf7320b
KavinthaBandara <kavinthahasaranga2@gmail.com>
2026-06-23 09:40:59 -0700

    changes fixed in the setttings now allowing 5174

--- Todo/Todo/settings.py
+++ Todo/Todo/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
+    "http://localhost:5174",
 ]
 
 #CORS_ALLOW_ALL_ORIGINS = True
--- Todo/core/urls.py
+++ Todo/core/urls.py
@@ -7,7 +7,7 @@ from .views import AddTodo, TodoDestroyAPIView, ViewTodo
 urlpatterns = [
     path('addtodo/', AddTodo.as_view(), name='add_todo'),
     path('viewtodo/', ViewTodo.as_view(), name='View_todo'),
-    path('<int: pk>/delete/', TodoDestroyAPIView.as_view(), name='todo-delete'),
+    path('<int:pk>/delete/', TodoDestroyAPIView.as_view(), name='todo-delete'),
 
 
 ]
